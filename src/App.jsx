89dd769e1e790fd176f26bcfcfb40cb725cd3712
Lycan-Xx <msbello514@gmail.com>
2024-11-06 15:54:18 +0100

    making the website structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './components/Header'
+import About from './components/About'
+import Services from './components/Services'
+import Testimonials from './components/Testimonials'
+
+function App() {
+    return (
+        <div className="className">
+            <Header />
+            <About />
+            <Services />
+            <Testimonials />
+
+        </div>
+    )
+}
+
+export default  App
