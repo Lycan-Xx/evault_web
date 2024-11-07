cdf33c1863286a1432aac1bbcb5eaa8793ac6e4a
Lycan-Xx <msbello514@gmail.com>
2024-11-07 19:52:13 +0100

    made some mistakes but fixed it

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,22 @@
-import React from "react";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
-import Features from "./components/Features";
-import Services from "./components/Services";
-import Testimonials from "./components/Testimonials";
-import FAQ from "./components/FAQ";
-import Footer from "./components/Footer";
+import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+import Services from './components/Services'
+import Testimonials from './components/Testimonials'
+import FAQ from './components/FAQ'
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="className">
-      <div>
-        <Navbar />
-        <div id="Home" style={{ height: "100vh", backgroundColor: "#f0f0f0" }}>
-          Home Section
-        </div>
-        <div
-          id="Services"
-          style={{ height: "100vh", backgroundColor: "#e0e0e0" }}
-        >
-          Services Section
-        </div>
-        <div id="About" style={{ height: "100vh", backgroundColor: "#d0d0d0" }}>
-          About Section
-        </div>
-        <div id="FAQ" style={{ height: "100vh", backgroundColor: "#c0c0c0" }}>
-          FAQ Section
-        </div>
-        <div
-          id="Contact"
-          style={{ height: "100vh", backgroundColor: "#b0b0b0" }}
-        >
-          Contact Section
-        </div>
-      </div>
-      <Navbar />
+    <div className="bg-white dark:bg-gray-900">
       <Hero />
-      <Features />
+      <About />
       <Services />
-
       <Testimonials />
       <FAQ />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
